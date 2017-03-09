f01a69e5b01428b5abf0b5b82cd7e42976da058c
sairamkonuru <sairam.konuru@gmail.com>
2017-03-09 06:26:10 +0530

    Add project files.

---
+++ ConsoleAppVs2017/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleAppVs2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//sreeraama
+           WriteLine("Jai Ganesha");
+           WriteLine("Jai Guru charan");
+           Beep();
+        }
+    }
+}
